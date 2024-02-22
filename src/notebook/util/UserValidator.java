d57a3e8c8cf702cd7da997e36e5bb6d1adb6a3a5
Yura <romanov.e180@gmail.com>
2024-02-22 18:43:44 +0300

    2

---
+++ src/notebook/util/UserValidator.java
@@ -0,0 +1,19 @@
+package notebook.util;
+
+import notebook.model.User;
+
+public class UserValidator {
+    public User validate(User user) {
+        if (!isValid(user)) throw new IllegalArgumentException("Ошибка валидации");
+        user.setFirstName(user.getFirstName().replaceAll(" "," ").trim());
+        user.setLastName(user.getLastName().replaceAll(" "," ").trim());
+        user.setPhone(user.getPhone().replaceAll(" "," ").trim());
+        return user;
+    }
+    private boolean isValid(User user) {
+        return !user.getFirstName().isEmpty()
+                && !user.getLastName().isEmpty()
+                && !user.getPhone().isEmpty();
+
+    }
+}
